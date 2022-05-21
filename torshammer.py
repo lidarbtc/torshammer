0beb39162a4baf6ddaddb0577e2d00bb30205304
lidar <97009593+lidarbtc@users.noreply.github.com>
2022-05-22 00:04:05 +0900

    Update torshammer.py

--- torshammer.py
+++ torshammer.py
@@ -1,12 +1,3 @@
-#!/usr/bin/python
-
-# this assumes you have the socks.py (http://phiral.net/socks.py) 
-# and terminal.py (http://phiral.net/terminal.py) in the
-# same directory and that you have tor running locally 
-# on port 9050. run with 128 to 256 threads to be effective.
-# kills apache 1.X with ~128, apache 2.X / IIS with ~256
-# not effective on nginx
-
 import os
 import re
 import time
